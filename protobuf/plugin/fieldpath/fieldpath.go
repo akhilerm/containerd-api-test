containerd/containerd

Introduce a new go module - containerd/api for use in standalone clients
In containerd 1.5.x, we introduced support for go modules by adding a
go.mod file in the root directory. This go.mod lists all the things
needed across the whole code base (with the exception of
integration/client which has its own go.mod). So when projects that
need to make calls to containerd API will pull in some code from
containerd/containerd, the `go mod` commands will add all the things
listed in the root go.mod to the projects go.mod file. This causes
some problems as the list of things needed to make a simple API call
is enormous. in effect, making a API call will pull everything that a
typical server needs as well as the root go.mod is all encompassing.
In general if we had smaller things folks could use, that will make it
easier by reducing the number of things that will end up in a consumers
go.mod file.

Now coming to a specific problem, the root containerd go.mod has various
k8s.io/* modules listed. Also kubernetes depends on containerd indirectly
via both moby/moby (working with docker maintainers seperately) and via
google/cadvisor. So when the kubernetes maintainers try to use latest
1.5.x containerd, they will see the kubernetes go.mod ending up depending
on the older version of kubernetes!

So if we can expose just the minimum things needed to make a client API
call then projects like cadvisor can adopt that instead of pulling in
the entire go.mod from containerd. Looking at the existing code in
cadvisor the minimum things needed would be the api/ directory from
containerd. Please see proof of concept here:
github.com/google/cadvisor/pull/2908

To enable that, in this PR, we add a go.mod file in api/ directory. we
split the Protobuild.yaml into two, one for just the things in api/
directory and the rest in the root directory. We adjust various targets
to build things correctly using `protobuild` and also ensure that we
end up with the same generated code as before as well. To

protobuf/plugin/fieldpath/fieldpath.go
@@ -19,7 +19,6 @@ package fieldpath
 import (
 	"strings"
 
-	"github.com/containerd/containerd/protobuf/plugin"
 	"github.com/gogo/protobuf/gogoproto"
 	"github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
 	"github.com/gogo/protobuf/protoc-gen-gogo/generator"
@@ -51,7 +50,7 @@ func (p *fieldpathGenerator) Generate(file *generator.FileDescriptor) {
 		if m.DescriptorProto.GetOptions().GetMapEntry() {
 			continue
 		}
-		if plugin.FieldpathEnabled(file.FileDescriptorProto, m.DescriptorProto) {
+		if FieldpathEnabled(file.FileDescriptorProto, m.DescriptorProto) {
 			p.generateMessage(m)
 		}
 	}

akhilerm/containerd-api-test

Containerd-test-commit: b9a98e207c342999f9215ae1551a494b6131f42a

protobuf/plugin/fieldpath/fieldpath.go
@@ -19,7 +19,6 @@ package fieldpath
 import (
 	"strings"
 
-	"github.com/containerd/containerd/protobuf/plugin"
 	"github.com/gogo/protobuf/gogoproto"
 	"github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
 	"github.com/gogo/protobuf/protoc-gen-gogo/generator"
@@ -51,7 +50,7 @@ func (p *fieldpathGenerator) Generate(file *generator.FileDescriptor) {
 		if m.DescriptorProto.GetOptions().GetMapEntry() {
 			continue
 		}
-		if plugin.FieldpathEnabled(file.FileDescriptorProto, m.DescriptorProto) {
+		if Enabled(file.FileDescriptorProto, m.DescriptorProto) {
 			p.generateMessage(m)
 		}
 	}